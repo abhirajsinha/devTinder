4788ae4907488f5dde2ebcf62cf51512b8b302d1
abhirajsinha <abhirajsinha25@gmail.com>
2025-04-18 14:34:18 +0530

    added mongoosw

---
+++ interview-readme.md
@@ -0,0 +1,5 @@
+# how express handle request internally
+# how next() works
+# what is middlewares
+# what is round handlers
+# difference between app.use() and app.all()
--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res, next) => {
-    res.send('hello world')
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Hanling Middlewares");
+    res.send("hello world");
+    next()
+  },
+  (req, res, next) => {
+    console.log("Handling Middlewares 2");
+    res.send('2nd Middleware')    
+  }
+);
 
 app.listen(3000, () => {
-  console.log("Server is listening on port: " + 3000)
+  console.log("Server is listening on port: " + 3000);
 });
